548e0d063f96090bd4754c8214def059a018a67b
Reshetniak Maksim <resetnak32@gmail.com>
2024-04-02 14:32:58 +0400

    init: added password encryption by Bcrypt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
@@ -17,18 +18,24 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation,async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
   const doc = new UserModel({
     email: req.body.email,
     fullName: req.body.fullName,
     avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.passwordHash,
+    passwordHash,
   });
+
+  
 
   res.json({
     success: true,
