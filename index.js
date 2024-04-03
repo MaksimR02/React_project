b120ea4301cba287a1974ed4608a520d0446fa15
Reshetniak Maksim <resetnak32@gmail.com>
2024-04-03 20:59:39 +0400

    init: added error handling and connect database by MongoDB Compass

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import UserModel from "./models/user.js";
 
 mongoose
   .connect(
-    "mongodb+srv://resetnak32:wwwwww@maksim.itlnk45.mongodb.net/?retryWrites=true&w=majority&appName=Maksim"
+    "mongodb+srv://resetnak32:wwwwww@maksim.itlnk45.mongodb.net/blog?retryWrites=true&w=majority&appName=Maksim"
   )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
@@ -18,28 +18,44 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation,async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign({
+      _id: user._id,
+    }, 'secret123',
+    {
+      expiresIn: '30d',
+    }
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  
-
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(4444, (err) => {
